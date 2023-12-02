d0dfe6a497802e06f6052ac7e34e12108841254d
Petr Lavrov <calmquant@gmail.com>
2023-12-02 04:11:02 +0300

    Loading version is apparently not that simple..

--- calmlib/__init__.py
+++ calmlib/__init__.py
@@ -9,10 +9,14 @@ except:
 
 from .utils.lib_discoverer import LibDiscoverer
 
-import toml
-from pathlib import Path
-
-path = Path(__file__).parent.parent / 'pyproject.toml'
-__version__ = toml.load(path)['tool']['poetry']['version']
-del toml
-del Path
+from importlib.metadata import PackageNotFoundError
+try:
+    import importlib.metadata
+    __version__ = importlib.metadata.version(__package__ or __name__)
+    del importlib
+except PackageNotFoundError:
+    import toml
+    from pathlib import Path
+    path = Path(__file__).parent.parent / 'pyproject.toml'
+    __version__ = toml.load(path)['tool']['poetry']['version']
+    del toml, Path, path
