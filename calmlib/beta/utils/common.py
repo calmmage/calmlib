ac25d64c4e4ccc55c1b7c80858febfd12985b927
Petr Lavrov <petrlavrov@calmmage.com>
2024-03-08 03:47:21 +0300

    Add copy_tree as it is removed from python 3.13

--- calmlib/beta/utils/common.py
+++ calmlib/beta/utils/common.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import shutil
+
+
 def trim(s, l=None, r=None):
     if l and s.startswith(l):
         s = s[len(l) :]
@@ -18,3 +22,20 @@ def is_subsequence(sub, main):
             sub_index += 1
         main_index += 1
     return sub_index == len(sub)
+
+
+def copy_tree(source, destination):
+    source_path = Path(source)
+    destination_path = Path(destination)
+
+    if not source_path.is_dir():
+        raise ValueError(f"Source ({source}) is not a directory.")
+
+    if not destination_path.exists():
+        destination_path.mkdir(parents=True)
+
+    for item in source_path.iterdir():
+        if item.is_dir():
+            copy_tree(item, destination_path / item.name)
+        else:
+            shutil.copy2(item, destination_path / item.name)
