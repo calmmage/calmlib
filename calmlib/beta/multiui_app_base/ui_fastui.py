e20a78fa46098cbcc9fbb1a5b528ddf568880089
Petr Lavrov <petrlavrov@calmmage.com>
2024-05-15 04:28:22 +0300

    [Beta.multi_app_base] Move app template to a new calmapp library

--- calmlib/beta/multiui_app_base/ui_fastui.py
+++
@@ -1,7 +0,0 @@
-"""
-Goal of this file:
-given an app that has a simple text-based interface, we want to create a FastUI + FastAPI interface for it
-app will have .invoke() method that takes a string and returns a string
-"""
-from app import App
-
