63609083c7c66f7e05413436cfd13198b5f7f18c
Reliable Magician <petrlavrov@calmmage.com>
2024-09-15 01:51:06 +0300

    Finish pyproject update and fix a few bugs

--- calmlib/utils/logging_utils/logging_utils.py
+++ calmlib/utils/logging_utils/logging_utils.py
@@ -1,6 +1,11 @@
+import logging
 from pathlib import Path
-
+from typing_extensions import deprecated
 from loguru import logger as loguru_logger
+
+DEFAULT_LEVEL = logging.INFO
+DEFAULT_SEP = " ~~ "
+DEFAULT_FORMAT_COMPONENTS = ["%(asctime)-15s", "%(name)s", "%(levelname)s", "%(message)s"]
 
 # Default log directory and file
 DEFAULT_LOG_DIR = Path("~/.calmmage/logs").expanduser()
@@ -40,4 +45,22 @@ def get_logger(name=None, level="INFO", format=None, log_file=None):
         logger.warning("Log file varaible is unused for now")
     if not is_logger_configured(logger):
         configure_calmmage_logger(logger)
+    return logger
+
+
+@deprecated("Use get_logger instead")
+def get_personal_logger(name, level=None, sep=None, format_components=None):
+    level = level or DEFAULT_LEVEL
+    sep = sep or DEFAULT_SEP
+    format_components = format_components or DEFAULT_FORMAT_COMPONENTS
+    import logging
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(fmt=sep.join(format_components))
+        handler.setFormatter(formatter)
+        # add the handlers to the logger
+        logger.addHandler(handler)
     return logger
