a2c6a1992ec38fcb1e4812851b1d70d0a4ac3c12
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-02-13 09:10:57 +0300

    Add setup_service command

--- calmlib/utils/service_registry/__init__.py
+++ calmlib/utils/service_registry/__init__.py
@@ -1,17 +1,13 @@
 """Service Registry utilities for monitoring service health via heartbeats"""
 
-from .heartbeat import (
-    heartbeat,
-    aheartbeat,
-    heartbeat_for_sync,
-    run_with_heartbeat,
-    get_api_url
-)
+from .heartbeat import heartbeat, aheartbeat, heartbeat_for_sync, run_with_heartbeat, get_api_url
+from .service_config import setup_service
 
 __all__ = [
-    'heartbeat',
-    'aheartbeat',
-    'heartbeat_for_sync',
-    'run_with_heartbeat',
-    'get_api_url'
-] 
+    "heartbeat",
+    "aheartbeat",
+    "heartbeat_for_sync",
+    "run_with_heartbeat",
+    "get_api_url",
+    "setup_service",
+]
